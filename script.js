25031162020d6bb2478bdb6817e99c410040f462
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-09-18 15:43:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
  $("#keyboard .key").click(function(){ console.log(this.id); })
 
+var startPos = 1000;
+
+var notes = function(value){
+	return this[value];
+};
+
+notes.a = {y : "55"};
+notes.b = {y : "60"};
 
 $(".game-container").html("<canvas id='canvas' height=100 width=" + $(this).innerWidth() + "></canvas>");
 var ctx = document.getElementById('canvas').getContext('2d');
-ctx.fillStyle="red";
-ctx.strokeStyle = "black";
-ctx.arc(1000, 55, 6, 0, 2 * Math.PI, false);
-ctx.fill();
-ctx.stroke();
+
+function note(ctx, x, y)
+{
+	ctx.fillStyle="red";
+	ctx.strokeStyle = "black";
+	ctx.arc(x, y, 6, 0, 2 * Math.PI, false);
+	ctx.fill();
+	ctx.stroke();
+}
+
+note(ctx, startPos, notes.a.y);
+note(ctx, startPos - 100, notes.b.y);

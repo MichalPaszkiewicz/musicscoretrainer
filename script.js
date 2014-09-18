d949c2aa4df89415a90f2f1a9bff8edb5c894cbb
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-09-18 15:36:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
  $("#keyboard .key").click(function(){ console.log(this.id); })
+
+
+$(".game-container").html("<canvas id='canvas' height=100 width=" + $(this).innerWidth() + "></canvas>");
+var ctx = document.getElementById('canvas').getContext('2d');
+ctx.fillStyle="red";
+ctx.strokeStyle = "black";
+ctx.arc(1000, 55, 6, 0, 2 * Math.PI, false);
+ctx.fill();
+ctx.stroke();

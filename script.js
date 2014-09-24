1231a4227c0a17cb13cf312246ea47a09c40ed8e
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-09-24 13:29:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -145,29 +145,6 @@ function updateScore()
 		stopGame();
 	}
 }
-
-  var audio_context, oscillator;
-
-  function stopSound() {
-    oscillator.noteOff(0);
-  }
-
-  function playSound(freq) {
-    oscillator = audio_context.createOscillator();
-    oscillator.frequency.value = freq;
-    oscillator.connect(audio_context.destination);
-    oscillator.noteOn(0);
-  }
-  
- (function init(g){
- 	try{
- 		audio_context = new(g.AudioContext || g.webkitAudioContext);
- 		oscillator = audio_context.createOscillator();
- 	} catch(e){
- 		alert("No web audio oscillator support in this browser. Use Chrome, fool!");
- 		soundIssues = true;
- 	}
- }(window));
   
 function playKey(key)
 {
